dd358c3450a83813b46c8e4bdc26f278f0c45b8f
AnnaBlok <black.doretie@yandex.ru>
2021-02-23 12:14:37 +0300

    Update first.php

--- first.php
+++ first.php
@@ -1,9 +1,8 @@
 <?php
 $fib = [0, 1];
 
-for($i = 1; $i < 64; $i++)
-    $fib[] = $fib[$i] + $fib[$i-1];
+for ($i = 1; $i < 64; $i++)
+    $fib[] = $fib[$i] + $fib[$i - 1];
  
-$numbers = implode(",", $fib);
+$numbers = implode(", ", $fib);
 echo $numbers;
-?>
