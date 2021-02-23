70d033c466ef5a6e3efdc3eb2fb322e854e828b6
AnnaBlok <black.doretie@yandex.ru>
2021-02-23 12:15:40 +0300

    Update third.php

--- third.php
+++ third.php
@@ -77,7 +77,7 @@ for ($i = 0; $i < count($fileArr); $i++) {
 }
 
 usort($fileArr, function($a, $b) {
-    return ($b['square']-$a['square']);
+    return ($b['square'] - $a['square']);
 });
 
 echo'<pre>', print_r($fileArr), '</pre>'; //можно использовать var_dump, но так более удобночитаемо и нет лишней информации
